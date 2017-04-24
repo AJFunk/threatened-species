a365f635af6453fb9ed4dd3ca2a7d85725864127
AJ Funk <aj.w.funk.9@gmail.com>
2017-04-24 08:40:02 -0700

    fetch and render species for specific countries

--- src/routes/Countries/components/Countries.js
+++ src/routes/Countries/components/Countries.js
@@ -8,24 +8,59 @@ class Countries extends Component {
     fetchCountries: PropTypes.func.isRequired
   }
 
+  constructor() {
+    super()
+    this.state = {
+      showCountry: false
+    }
+    this.fetchAndShowCountry = this.fetchAndShowCountry.bind(this)
+    this.renderLoading = this.renderLoading.bind(this)
+    this.backToCountries = this.backToCountries.bind(this)
+  }
+
   componentDidMount() {
-    if(!this.props.countries.loaded) this.props.fetchCountries();
+    if(!this.props.countries.loaded) this.props.fetchCountries()
+  }
+
+  fetchAndShowCountry(name, isocode) {
+    this.props.fetchCountry(name, isocode)
+    this.setState({ showCountry: true })
+  }
+
+  backToCountries() {
+    this.setState({ showCountry: false })
+  }
+
+  renderLoading() {
+    return (
+      <div>
+        <h1>LOADING</h1>
+      </div>
+    )
+  }
+
+  renderCountry() {
+    const { country } = this.props.countries
+    if(country.loading) return this.renderLoading()
+    return (
+      <div>
+        <button className='btn btn-default' onClick={this.backToCountries}>Back to Countries</button>
+        <h1>{country.name}</h1>
+        {country.species.map((e,i) => <div key={i}>{e.scientific_name} ({e.category}) - #{e.taxonid}</div>)}
+      </div>
+    )
   }
 
   render() {
     const { loading, countries} = this.props.countries
 
-    if(loading) {
-      return (
-        <div>
-          <h1>LOADING</h1>
-        </div>
-      )
-    }
+    if(loading) return this.renderLoading()
+    if(this.state.showCountry) return this.renderCountry()
+
     return (
       <div>
         <h3>Countries</h3>
-        {countries.map((e,i) => <div key={i} onClick={this.props.fetchCountry.bind(this, e.country, e.isocode)}>{e.country}</div>)}
+        {countries.map((e,i) => <div key={i} onClick={this.fetchAndShowCountry.bind(this, e.country, e.isocode)}>{e.country}</div>)}
       </div>
     )
   }
